4a48492bcfd33b5d518e4353db2d5fc04d233139
Amrit Chalise <145205962+amritchalise@users.noreply.github.com>
2024-02-22 11:46:11 -0500

    Delete my-portfolio directory

--- my-portfolio/src/About.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './App.css';
-const About = () => (
-  <div className="about">
-    <h2>About Me</h2>
-    <p>I'm Amrit Chalise, a passionate full-stack developer based in Toronto. I love building web applications and exploring new technologies.</p>
-  </div>
-);
-export default About;
